364a52e30b0c70c3e9a7f877e071654470a793b8
daniel <dan.pascal@gmail.com>
2017-02-10 11:04:11 +0100

    ajout todo

--- modele/jsonCid.php
+++ modele/jsonCid.php
@@ -6,7 +6,7 @@ $arrFrx=array();
 if (isset($_GET['code']))
 	{$reponse = $ptirex->prepare('SELECT numrex AS reference, titre, date, type, site, palier notes FROM cid LEFT JOIN codescid ON cid.numrex = codescid.reference WHERE code=? AND (dateevt BETWEEN ? AND ?)');
 	$reponse->execute(array($_GET['code'], $_SESSION["debut"], $_SESSION["fin"]));}
-
+		//todo, mettre notes en 3 pour mutualiser avec jsonfrx
 		//conserver reference en premier (adh�rence avec le JS pour le lien vers la reference), conserver notes en 7ème pour adhérence js
 
 while ($donnees = $reponse->fetch())
