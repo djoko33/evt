bf65b8b119f2a4f12813e9666b3766fe7aa61294
daniel <dan.pascal@gmail.com>
2017-02-10 11:02:39 +0100

    modif pour avoir 7 champs de retour

--- modele/jsonCid.php
+++ modele/jsonCid.php
@@ -4,10 +4,10 @@ include('connexionPtirex.php');
 
 $arrFrx=array();
 if (isset($_GET['code']))
-	{$reponse = $ptirex->prepare('SELECT numrex AS reference, titre, date, type, notes FROM cid LEFT JOIN codescid ON cid.numrex = codescid.reference WHERE code=? AND (dateevt BETWEEN ? AND ?)');
+	{$reponse = $ptirex->prepare('SELECT numrex AS reference, titre, date, type, site, palier notes FROM cid LEFT JOIN codescid ON cid.numrex = codescid.reference WHERE code=? AND (dateevt BETWEEN ? AND ?)');
 	$reponse->execute(array($_GET['code'], $_SESSION["debut"], $_SESSION["fin"]));}
 
-		//conserver reference en premier (adh�rence avec le JS pour le lien vers la reference)
+		//conserver reference en premier (adh�rence avec le JS pour le lien vers la reference), conserver notes en 7ème pour adhérence js
 
 while ($donnees = $reponse->fetch())
 {
